7a4eccdc940e8fb3b3cfea43b184d0a667dd68b2
xiaohu <xiaohu.zhang@ruhlamat.com.cn>
2026-04-29 07:06:46 +0800

    Remove unused scripts related to test case management and element key comparison
    - Deleted `export.py` which handled loading test cases from JSON and writing to Excel.
    - Removed `use_compare_elem_key.py` that extracted and cleaned unused keys from element positioning.
    - Eliminated `use_match_key.py` which identified duplicate values in element positioning.
    - Cleared `use_testcase_number.py` responsible for managing test case numbering and duplicates.
    - Discarded `use_testcase_statics_num.py` that summarized test case statistics based on decorators.

--- webseleniumerp/pages/pages_mall.py
+++
@@ -1,8 +0,0 @@
-# coding: utf-8
-import os
-from config.settings import DATA_PATHS
-from common.base_page import BasePage, ImportDataEdit
-from common.import_desc import *
-
-
-
--- webseleniumerp/request/request_login.py
+++
@@ -1,8 +0,0 @@
-# coding: utf-8
-from common.base_api import BaseApi
-from common.base_params import InitializeParams
-
-
-class HdH5D8EWBQ(InitializeParams):
-    """登录"""
-
